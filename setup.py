a63f9265324673778a351fae32004f69d6ad5592
Denis Korytkin <denis.korytkin@datarobot.com>
2020-03-25 19:09:32 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pytest-sherlock",
-    version="0.1.1",
+    version="0.1.2",
     author="Denis Korytkin",
     author_email="dkorytkin@gmail.com",
     description="pytest plugin help to find coupled tests",
